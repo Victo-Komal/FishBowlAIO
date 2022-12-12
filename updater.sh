47eb738662bae43ce894afb259e9bddab8395393
42 <jvkt@outlook.dk>
2022-12-12 12:34:24 +0100

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -2,7 +2,12 @@ sudo apt-get update
 sudo apt-get install git
 sudo apt install python3-pip
 sudo apt-get install arduino
-sudo apt-get install code
 pip3 install --upgrade pip
 pip3 install paho-mqtt
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
 git clone http://github.com/Victo-Komal/FishBowlAIO.git
+
+
