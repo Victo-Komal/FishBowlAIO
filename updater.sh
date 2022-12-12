921a35fc3c163bcdfb8ad95d26d429458f291cbd
42 <jvkt@outlook.dk>
2022-12-12 12:00:49 +0100

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,6 +1,8 @@
 sudo apt-get update
 sudo apt-get install git
 sudo apt-get install pip3
+sudo apt-get install arduino
+sudo apt install code
 pip3 install --upgrade pip
 pip3 install paho-mqtt
 git clone http://github.com/Victo-Komal/FishBowlAIO.git
